fc623b566f1d40f5ba1213486e882f0e7666237f
Thomas Aynaud <thomas@clustree.com>
2017-02-22 10:10:06 +0100

    Update for networkx 2.0

--- test_community.py
+++ test_community.py
@@ -192,7 +192,7 @@ class BestPartitionTest(unittest.TestCase):
         part = co.best_partition(graph)
         self.assertTrue(co.modularity(part, graph) > 0.41)
 
-        for e1, e2 in graph.edges_iter():
+        for e1, e2 in graph.edges():
             graph[e1][e2]["test_weight"] = 1.
 
         part_weight = co.best_partition(graph, weight="test_weight")
@@ -230,8 +230,8 @@ class InducedGraphTest(unittest.TestCase):
         self.assertEqual(graph.size(weight='weight'),
                          co.induced_graph(part, graph).size(weight='weight'))
 
-        for e1, e2 in graph.edges_iter():
-            graph[e1][e2]["test_weight"] = 2.
+        for e1, e2 in graph.edges():
+            graph[e1][e2]["test_weight"] = 1.
 
         self.assertEqual(graph.size(weight='test_weight'),
                          co.induced_graph(part, graph, "test_weight").size(weight='test_weight'))
